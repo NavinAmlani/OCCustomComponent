6e9f16e9534168703d6686330397c74c090609b5
Navin <navin@itoneclick.com>
2021-06-11 11:24:56 +0530

    modify files

--- Classes/CustomToast.swift
+++ Classes/CustomToast.swift
@@ -25,7 +25,7 @@ extension UILabel
     
     override open func draw(_ rect: CGRect) {
         if let insets = padding {
-            self.drawText(in: UIEdgeInsetsInsetRect(rect, insets))
+            self.drawText(in: rect.inset(by: insets))
         } else {
             self.drawText(in: rect)
         }
