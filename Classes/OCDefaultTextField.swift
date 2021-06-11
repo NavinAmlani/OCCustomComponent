b5b50b03f2a539decfa80b3c5b751349a949effc
Navin <navin@itoneclick.com>
2021-06-11 12:23:09 +0530

    add method in textfield file

--- Classes/OCDefaultTextField.swift
+++ Classes/OCDefaultTextField.swift
@@ -316,12 +316,12 @@ extension UITextField{
     
     //MARK:- For padding left and Right
     
-    func setLeftPaddingPoints(_ amount:CGFloat){
+    public func setLeftPaddingPoints(_ amount:CGFloat){
             let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
             self.leftView = paddingView
             self.leftViewMode = .always
         }
-        func setRightPaddingPoints(_ amount:CGFloat) {
+      public func setRightPaddingPoints(_ amount:CGFloat) {
             let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
             self.rightView = paddingView
             self.rightViewMode = .always
